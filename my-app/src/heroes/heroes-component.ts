1230cc40083965df401e72f76d548936d4072e0c
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2020-12-12 19:12:21 +0900

    クリックイベントを紐づけた

--- my-app/src/heroes/heroes-component.ts
+++ my-app/src/heroes/heroes-component.ts
@@ -1,7 +1,13 @@
 import { Hero } from '../models/hero';
+import { HEROES } from '../mock-heroes';
 export class HeroesComponent {
+    heroes = HEROES;
     hero: Hero = {
         id: 1,
         name: 'Windstorm'
     };
+    selectedHero: Hero;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
---
+++ my-app/src/mock-heroes.ts
@@ -0,0 +1,6 @@
+import { Hero } from './models/hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice'},
+    { id: 12, name: 'Narco'}
+];
